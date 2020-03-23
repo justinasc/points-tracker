f677d5c6cdff87e2d1cef6c376ef76e20f5ff11e
Justinas Ci <justinas.ciucelis@gmail.com>
2020-03-23 21:32:42 +0200

    Removed back-end because my wife doesn't want any functionality :(

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php include_once 'config/init.php'; ?>
 
 <?php
-$points = new Points;
-
 $template = new Template('templates/main.php');
-
-$template->title = 'Title';
-$template->points = $points->getAllPoints();
 
 echo $template;
--- templates/main.php
+++ templates/main.php
@@ -1,24 +1,34 @@
 <?php include 'inc/header.php';?>
 
-<?php foreach($points as $point): ?>
 
 <div id="wrapper">	
+	<div class="title">
+		<svg viewBox="0 -40 500 500">
+			<path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+			<text width="500">
+			<textPath xlink:href="#curve">
+				Team Norway
+			</textPath>
+			</text>
+			
+		</svg>
+	</div>
+	<img src="./images/viking.png" alt="">
 	<div id="termometer">
 		<div id="points" style="height:0" data-value="0 Points"></div>
-		<div id="goal-1" style="height:28%" data-value="350 Points"></div>
-		<div id="goal-2" style="height:56%" data-value="700 Points"></div>
+		<div id="goal" style="height:28%" data-value="Team break - 350 Points"></div>
+		<div id="goal" style="height:56%" data-value="Team lunch - 700 Points"></div>
+		<div id="goal" style="height:100%" data-value="Team &quot;out&quot; - 1250 Points"></div>
 		<div id="graduations"></div>
 	</div>
 	
-	<div id="playground">		
+	<div id="playground" style="display:none">		
 		<div id="range">
 			<input id="minPoints" type="text" value="0">
-			<input type="range" min="0" max="1250" value="<?php echo $point->points ?>">
+			<input type="range" min="0" max="1250" value="200">
 			<input id="maxPoints" type="text" value="1250">
 		</div>
 	</div>
-	
-<?php endforeach; ?> 
 
 </div>
 <script src="js/script.js"></script>
